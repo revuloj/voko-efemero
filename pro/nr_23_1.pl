f03624d3bc4edc228ceb446e1d937591a72ef837
Wolfram Diestel <wolfram.diestel@t-systems.com>
2023-11-03 16:14:51 +0100

    db ŝovita, +trd zh d

---
+++ pro/nr_23_1.pl
@@ -0,0 +1,16 @@
+% kio estas Prologo?
+prologo(programlingvo).
+prologo(solvilo).
+prologo(datumbazo).
+prologo(serĉilo).
+
+estas(predikato,tabelo).
+estas(fakto,datenopo).
+estas(fakto,rikordo).
+estas(variablo,variablo).
+estas(atomo,nomo).
+estas(regulo,'Kapo :- Korpo.').
+estas(kapo,termo).
+estas(korpo,termo).
+estas(kapo,predikato).
+estas(korpo,esprimo).
