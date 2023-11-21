2c60064fb2bf53cb48e00fdf71d50d4911460049
Wolfram Diestel <diestel@steloj.de>
2023-11-21 17:08:44 +0100

    +trd zh k

--- pro/nr_23_1.pl
+++ pro/nr_23_1.pl
@@ -1,11 +1,34 @@
-% faktoj pri Prologo
+/**
+ Ponta vortaro en kvin paŝoj
+
+ 1. Enkonduko en Prologon
+ 2. Legi vortaron, rilatigi vortojn
+ 3. Tradukparoj kaj -pontoj
+ 4. Esperanto-ĉina vortaro
+ 5. Servi retpaĝon
+
+*/
+
+
+% faktoj pri Prologo k.a.
 % pridemandebla per ?- speco('Prologo',X)
+% SQL-ekvivalento: 
+% SELECT * FROM speco WHERE subjekto = 'Prologo';
+%
+% (variablo-nomoj estas majusklaj, do tekstvaloroj
+% estu aŭ minusklaj aŭ intercitilaj)
 speco('Prologo',programlingvo).
 speco('Prologo',solvilo).
 speco('Prologo',datumbazo).
 speco('Prologo',serĉilo).
+speco('Paskalo',programlingvo).
+speco('Pitono',programlingvo).
+speco('Perlo',programlingvo).
+speco('Orakolo',datumbazo).
 
 % faktoj pri lingvoj
+% la sintakso en SQL estus
+% INSERT INTO speco VALUES ('angla,'nacilingvo'); -- ktp.
 speco(angla,nacilingvo).
 speco(franca,nacilingvo).
 speco(germana,nacilingvo).
@@ -24,6 +47,10 @@ speco(urso,rabobesto).
  * ?- speco(L,lingvo) ne donas rezulton, manke de faktoj, sed ni
  * povas difini regulon:
  * Lingvo povas esti nacilingvo aŭ internacilingvo aŭ programlingvo
+ *
+ * SQL-ekvivalento:
+ * SELECT subjekto FROM speco WHERE speco = 'nacilingvo'
+ *     OR speco = 'internacilingvo' OR speco = 'programlingvo'
  */
 lingvo(L) :-
     speco(L,nacilingvo)
@@ -49,6 +76,7 @@ besto(B) :-
  * Ankaŭ ĉiuj mamuloj, hundoj ktp. estas bestoj. Ni bezonas pli vastan
  * difino por besto do:
  * Ĉio, kies speco siavice estas besto, estas besto (rikura difino)
+ * Kontrola demando: kial la rikuro funkcias? Kial ĝi ne kuras eterne?
  */
 besto2(B) :- 
     besto(B).
